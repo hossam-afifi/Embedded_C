309544b8524dc0d33e61fed546a32d0ad5858414
hossam_afifi <eng-hossam.magdy773@gmail.com>
2021-08-30 16:24:50 +0200

    unit3_lesson2

---
+++ assignment3/unit3_lesson2/APP.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "uart.h"
+
+unsigned char  const string_buffer_1[100]  = "Welcome\n";
+
+unsigned const char string_buffer[100]  = "Hello Hossam Magdy --> learn in depth";
+
+
+int main () {
+	uart_send_string( string_buffer_1 );
+	
+	uart_send_string( string_buffer );
+	return 0;
+}
